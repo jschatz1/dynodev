078f5dd22143ab5ad561f42fece6a54ce114821d
Jacob Schatz <jschatz1@gmail.com>
2020-05-13 21:00:54 +0000

    remove api to replace with rails

--- packages/api/src/helpers/envHelpers.js
+++
@@ -1,4 +0,0 @@
-module.exports.isDev = process.env.NODE_ENV === "development";
-module.exports.isTest = process.env.NODE_ENV === "test";
-module.exports.isStaging = process.env.NODE_ENV === "staging";
-module.exports.isProd = process.env.NODE_ENV === "production";
--- packages/api/src/modules/projects/projects.routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express");
-const projectsController = require("./projects.controller");
-const modelsController = require("../models/models.controller");
-const routes = new Router();
-
-routes.post("/", projectsController.create);
-routes.get("/:uuid/models", modelsController.show);
-
-module.exports = routes;
